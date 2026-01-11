36f21a43353a71b7735a8dd8da33e3bffca51012
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-01-11 09:37:36 +0200

    colors

---
+++ constants/colors.js
@@ -0,0 +1,16 @@
+export const MEMORA_COLORS = {
+    primary: '#e687a2',      
+    primaryDark: '#bf5c7a',  
+    accent: '#E8C39E',       
+    background: '#edc0de',  
+    backgroundLight: '#F8E1E9', 
+    textPrimary: '#4A2C35', 
+    textSecondary: '#6B4A55',
+    white: '#FFFFFF',
+    black: '#000000',
+    error: '#C94C4C',
+    success: '#6BAF92',
+
+    gradientStart: '#F8E1E9',
+    gradientEnd: '#E8C7D3',
+};
