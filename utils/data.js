5df86ddb1165f4d2f2fc03b7df8ff1a0e2633985
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-01-12 04:59:14 +0200

    update app configuration and add data utility functions

---
+++ utils/data.js
@@ -0,0 +1,6 @@
+import occasionsAndProdacts from "../app.json";
+
+export const occasions= occasionsAndProdacts.occasions;
+export const  products=occasionsAndProdacts.products;
+
+console.log( occasions,products);
