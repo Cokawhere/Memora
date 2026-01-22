ed3e47c9aec55300026b1f8b9badd5a06ee36eab
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-01-22 12:19:54 +0200

    Add EmptyFavorites component to display message when no favorites are present

---
+++ components/EmptyFavorites.js
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet } from "react-native";
+import { COLORS } from "../constants/colors";
+
+export default function EmptyFavorites() {
+return (
+        <View style={styles.emptyContainer}>
+            <Text style={styles.emptyTitle}>
+                No favorites yet 
+            </Text>
+            <Text style={styles.emptyText}>
+                Go and grab some products you love 
+            </Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    emptyContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+        marginTop:300,
+    },
+    emptyTitle: {
+        fontSize: 22,
+        fontWeight: "bold",
+        color: COLORS.primary,
+        marginBottom: 8,
+    },
+    emptyText: {
+        fontSize: 16,
+        color: COLORS.primaryDark,
+        textAlign: "center",
+    },
+});
