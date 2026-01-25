34ae8fe3b06d72bcfced0e24e68cb1e66d4980ed
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-01-25 13:38:44 +0200

    move native-stack from

---
+++ navigation/Native-stack.js
@@ -0,0 +1,44 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OccasionOverViewScreen from "../screens/OccasionOverViewScreen";
+import ProductDetailsScreen from "../screens/ProductDetailsScreen";
+import DrawerNavigation from "../navigation/Drawer"
+import { COLORS } from "../constants/colors";
+
+const Stack = createNativeStackNavigator();
+export default function Nativestack() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: COLORS.primary,
+                },
+                headerTintColor: COLORS.white,
+                headerTitleAlign: "center",
+                headerTitleStyle: {
+                    fontSize: 25,
+                    fontWeight: "bold",
+                },
+            }}
+        >
+            <Stack.Screen
+                name="drawer"
+                component={DrawerNavigation}
+                options={{
+                    title: "Home",
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="occsionDetails"
+                component={OccasionOverViewScreen}
+            />
+            <Stack.Screen
+                name="productDetails"
+                component={ProductDetailsScreen}
+                options={{
+                    title: "Product Details",
+                }}
+            />
+        </Stack.Navigator>
+    )
+}
