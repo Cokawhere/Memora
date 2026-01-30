9141b2652c1dbbc4d587907629b6d9f0546c24c4
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-01-30 09:31:42 +0200

    edit colors for dark and light

--- constants/colors.js
+++ constants/colors.js
@@ -1,4 +1,5 @@
-export const COLORS = {
+
+export const lightTheme =  {
     primary: '#efb9c8',
     primaryDark: '#bf5c7a',
     accent: '#E8C39E',
@@ -10,8 +11,21 @@ export const COLORS = {
     black: '#000000',
     error: '#C94C4C',
     success: '#6BAF92',
-   
-
+    gradientStart: '#F8E1E9',
+    gradientEnd: '#E8C7D3',
+};
+export const darkTheme =  {
+    primary: '#efb9c8',
+    primaryDark: '#bf5c7a',
+    accent: '#E8C39E',
+    background: '#edc0de',
+    backgroundLight: '#f6ebef',
+    textPrimary: '#4A2C35',
+    textSecondary: '#6B4A55',
+    white: '#FFFFFF',
+    black: '#000000',
+    error: '#C94C4C',
+    success: '#6BAF92',
     gradientStart: '#F8E1E9',
     gradientEnd: '#E8C7D3',
 };
