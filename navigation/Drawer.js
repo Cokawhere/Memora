969d65d761adfdc4d1148066c432a63f95fd7d56
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-02-03 01:43:26 +0200

    changing drawer width.

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -5,13 +5,25 @@ import { Ionicons } from "@expo/vector-icons";
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import CustomDrawerContent from "../components/CustomDrawerContent";
+import { SafeAreaView } from "react-native-safe-area-context";
 const Drawer = createDrawerNavigator();
 export default function DrawerNavigation() {
     const { COLORS } = useContext(ThemeContext);
     if (!COLORS) return null;
     return (
         <Drawer.Navigator
-            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            drawerContent={(props) => (
+                <SafeAreaView
+                    style={{
+                        flex: 1,
+                        backgroundColor: COLORS.primary,
+                        borderTopRightRadius: 20,
+                        borderBottomRightRadius: 20,
+                        overflow: "hidden",
+                    }}>
+                    <CustomDrawerContent {...props} />
+                </SafeAreaView>
+            )}
             screenOptions={{
                 headerStyle: {
                     backgroundColor: COLORS.primary,
@@ -22,9 +34,14 @@ export default function DrawerNavigation() {
                     fontSize: 25,
                     fontWeight: "bold",
                 },
+                drawerStyle: { width: 280 },
+                overlayColor: "rgba(0, 0, 0, 0.21)",
                 drawerContentStyle: {
                     backgroundColor: COLORS.primary,
+                    overflow: "hidden",
+
                 },
+
                 drawerInactiveTintColor: COLORS.white,
                 drawerActiveTintColor: COLORS.background,
                 drawerActiveBackgroundColor: COLORS.primaryDark,
