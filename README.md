2ed5676c578e68356b316bb3dab53838b7ed6f5c
cokawhere_GitHub <ranaallii0.88@gmail.com>
2026-02-03 20:07:09 +0200

    replace useState with useReducer

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Memora 💍
+# Memora 
 
 React Native mobile app for discovering and saving occasion decorations (engagements, katb el ketab, birthdays, baby showers, graduations, anniversaries, etc.).
-
 Built with clean code, beautiful UI inspired by Pinterest, and smooth performance .
 
 ## Screenshots
@@ -62,14 +61,14 @@ Built with clean code, beautiful UI inspired by Pinterest, and smooth performanc
 
 ## Tech & Implementation Highlights
 
-| Technology / Pattern                | Description                                                                    | 
-| ----------------------------------- | ------------------------------------------------------------------------------ |
-| **Expo + Dev Client**               | Built & run using `expo-dev-client` for native modules support                 |          |
+| Technology / Pattern                | Description                                                                    |
+| ----------------------------------- | ------------------------------------------------------------------------------ | --- |
+| **Expo + Dev Client**               | Built & run using `expo-dev-client` for native modules support                 |     |
 | **React Navigation — Native Stack** | `@react-navigation/native-stack` for smooth, native-feeling screen transitions |
-| **Custom Hook: useFavorites**       | Manages favorites list + AsyncStorage persistence                              | 
-| **AsyncStorage** (local storage)    | Persistent favorites (can be swapped later with MMKV for speed)                | 
-| **FlashList (masonry)**             | `@shopify/flash-list` with `masonry={true}` & `numColumns={2}`                 | 
-| **Dynamic Image Aspect Ratio**      | `Image.getSize()` + `aspectRatio` style to prevent stretching                  | 
-| **Splash Screen**                   | Customized via `expo-splash-screen` + `app.json` config                        | 
-| **Consistent Theming**              | Central `colors.js` file + shadow/text effects for elegant UI                  | 
-| **Pinterest-inspired Design**       | Rounded corners, overlays, text shadows, masonry layout, big hero images       | 
+| **Custom Hook: useFavorites**       | Manages favorites list + AsyncStorage persistence                              |
+| **AsyncStorage** (local storage)    | Persistent favorites (can be swapped later with MMKV for speed)                |
+| **FlashList (masonry)**             | `@shopify/flash-list` with `masonry={true}` & `numColumns={2}`                 |
+| **Dynamic Image Aspect Ratio**      | `Image.getSize()` + `aspectRatio` style to prevent stretching                  |
+| **Splash Screen**                   | Customized via `expo-splash-screen` + `app.json` config                        |
+| **Consistent Theming**              | Central `colors.js` file + shadow/text effects for elegant UI                  |
+| **Pinterest-inspired Design**       | Rounded corners, overlays, text shadows, masonry layout, big hero images       |
--- contexts/ThemeContext.js
+++ contexts/ThemeContext.js
@@ -1,26 +1,48 @@
 import { View, Text } from 'react-native'
-import { createContext, useState, useMemo, useEffect,useContext } from 'react'
+import { createContext, useReducer, useMemo, useEffect, useContext } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { darkTheme, lightTheme } from '../constants/colors';
 
 
 const THEMS_KEY = 'memora_theme';
 export const ThemeContext = createContext();
-function useTheme (){
-    const context=useContext(ThemeContext);
-    if(context===undefined ) throw new Error('context is used out of his scope');
+function useTheme() {
+    const context = useContext(ThemeContext);
+    if (context === undefined) throw new Error('context is used out of his scope');
     return context;
-} 
+}
+const initialState = {
+    isDark: false,
+    COLORS: lightTheme
+}
+function themeReducer(state, action) {
+    switch (action.type) {
+        case "setTheme":
+            return {
+                ...state,
+                isDark: action.payLoad,
+                COLORS: action.payLoad ? darkTheme : lightTheme,
+            };
+        case "ToggleTheme":
+            const newTheme = !state.isDark;
+            return {
+                ...state,
+                isDark: newTheme,
+                COLORS: newTheme ? darkTheme : lightTheme,
+            }
+        default:
+            return state;
+    }
+};
 
 export default function ThemeProvider({ children }) {
-    const [isDark, setIsDark] = useState(false);
-
+    const [state, dispatch] = useReducer(themeReducer, initialState)
     useEffect(() => {
         const loadTheme = async () => {
             try {
                 const savedTheme = await AsyncStorage.getItem(THEMS_KEY);
                 if (savedTheme !== null) {
-                    setIsDark(JSON.parse(savedTheme));
+                    dispatch({ type: "setTheme", payLoad: JSON.parse(savedTheme) });
                 }
             } catch (e) {
                 console.log(`faild to load the current theme ${e}`)
@@ -29,22 +51,25 @@ export default function ThemeProvider({ children }) {
         loadTheme();
     }, []);
 
-    const toggleTheme = async () => {
-        const newTheme = !isDark;
-        setIsDark(newTheme);
-        try {
-            await AsyncStorage.setItem(THEMS_KEY, JSON.stringify(newTheme));
+    useEffect(() => {
+        (async () => {
+            try {
+                await AsyncStorage.setItem(THEMS_KEY, JSON.stringify(state.isDark));
 
-        } catch (e) {
-            console.log(`faild to change the theme please try again ${e}`)
+            } catch (e) {
+                console.log(`faild to change the theme please try again ${e}`)
+            }
+        })();
+    }
+        , [state.isDark]);
 
-        }
-    };
+    const toggleTheme = () => dispatch({ type: "ToggleTheme" });
+
     const themeValue = useMemo(() => ({
-        isDark, 
+        isDark: state.isDark,
         toggleTheme,
-        COLORS:isDark?darkTheme : lightTheme,
-    }), [isDark])
+        COLORS: state.isDark ? darkTheme : lightTheme,
+    }), [state.isDark]);
 
     return (
         <ThemeContext.Provider value={themeValue}>
@@ -52,4 +77,4 @@ export default function ThemeProvider({ children }) {
         </ThemeContext.Provider>
     )
 }
-export {useTheme};
+export { useTheme };
